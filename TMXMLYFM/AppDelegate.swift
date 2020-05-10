df81e70aac4bcc8f711e27e710e68187e99d81f8
wubingjia <my_badboy521@163.com>
2020-05-10 15:46:16 +0800

    first commit

---
+++ TMXMLYFM/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TMXMLYFM
+//
+//  Created by Luther on 2020/5/10.
+//  Copyright © 2020 mrstock. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ TMXMLYFM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMXMLYFM
+//
+//  Created by Luther on 2020/5/10.
+//  Copyright © 2020 mrstock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
